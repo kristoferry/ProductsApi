4e079c3f957e27fa3e3b91b89d298920dab30531
Kristofer Rydberg <kristoferry@gmail.com>
2018-02-08 22:04:22 +0100

    Initial commit

---
+++ Models/ProductItem.cs
@@ -0,0 +1,13 @@
+namespace ProductsApi.Models {
+
+    public class ProductItem{
+        long productId;
+        string productName;
+        string productCode;
+        string releaseDate;
+        double price;
+        string description;
+        int starRating;
+        string imageUrl;
+    }
+}
